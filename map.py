a681e61e065e30b0cd40dfd77c5a96645a50ae59
pavakta <pavakta@myseneca.ca>
2020-06-08 14:39:01 -0400

    change the marker style to circle
    the circle color is picked dynamically depending on the height of the volcano.

--- map.py
+++ map.py
@@ -13,10 +13,24 @@ html = """
         Height: %s m
         """
 
+def pick_color(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
 usa_map = folium.Map(location=[40, -100], zoom_start=4, tiles='Stamen Terrain')
 
 for lt, ln, el, name in zip(lat, lon, elv, nm):
     iframe = folium.IFrame(html=html % (name, name, el), width=200, height=80)
-    folium.Marker([lt, ln], popup=folium.Popup(iframe)).add_to(usa_map)
+    folium.CircleMarker(
+        location = [lt, ln],
+        popup=folium.Popup(iframe),
+        radius=5,
+        stroke=False,
+        fill_opacity=0.7,
+        fill_color=pick_color(el)).add_to(usa_map)
 
 usa_map.save('index.html')
