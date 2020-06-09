7965c7a087eb031bd07467eedef00f37ddcd7b18
pavakta <pavakta@myseneca.ca>
2020-06-09 17:33:31 -0400

    Add layer control to the map

--- map.py
+++ map.py
@@ -29,9 +29,10 @@ def pop_color(pop):
     else:
         return 'red'
 
-usa_map = folium.Map(location=[40, -100], zoom_start=4, tiles='Stamen Terrain')
+usa_map = folium.Map(location=[40, -100], zoom_start=4, tiles='Stamen Toner')
 
-fg = folium.FeatureGroup(name='My Map')
+fgv = folium.FeatureGroup(name='Volcanoes')
+fgp = folium.FeatureGroup(name='Population')
 
 for lt, ln, el, name in zip(lat, lon, elv, nm):
     iframe = folium.IFrame(html=html % (name, name, el), width=200, height=80)
@@ -42,11 +43,14 @@ for lt, ln, el, name in zip(lat, lon, elv, nm):
         stroke=False,
         fill_opacity=0.7,
         fill_color=elev_color(el))
-    fg.add_child(markers)
+    fgv.add_child(markers)
 
-fg.add_child(folium.GeoJson(data=(open('world.json', 'r', encoding='utf-8-sig').read()),
+fgp.add_child(folium.GeoJson(data=(open('world.json', 'r', encoding='utf-8-sig').read()),
     style_function= lambda x: {'fillColor': pop_color(x['properties']['POP2005'])}))
 
-usa_map.add_child(fg)
+usa_map.add_child(fgv)
+usa_map.add_child(fgp)
+
+usa_map.add_child(folium.LayerControl())
 
 usa_map.save('index.html')
